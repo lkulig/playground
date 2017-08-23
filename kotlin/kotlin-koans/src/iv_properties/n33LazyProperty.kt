9ae3c0e6e9579608ab9f128bf575d9c0c370d3ab
Łukasz Kulig <lukasz.kulig88@gmail.com>
2017-08-23 19:44:53 +0200

    Kotlin Koans - properties

--- kotlin/kotlin-koans/src/iv_properties/n33LazyProperty.kt
+++ kotlin/kotlin-koans/src/iv_properties/n33LazyProperty.kt
@@ -3,7 +3,15 @@ package iv_properties
 import util.TODO
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    private val value: Int? = null
+        get() {
+            if(field == null) field = initializer()
+            return field
+        }
+
+
+    val lazy: Int
+        get() = value!!
 }
 
 fun todoTask33(): Nothing = TODO(
